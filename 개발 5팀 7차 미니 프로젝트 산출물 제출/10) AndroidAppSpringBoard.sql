b7785058ec10fd7bf906d81c8233004b4252229f
yunjj92 <yunjj92@gmail.com>
2021-05-14 21:04:45 +0900

    5월15일까지 완성 필요

--- 개발 5팀 7차 미니 프로젝트 산출물 제출/10) AndroidAppSpringBoard.sql
+++ 개발 5팀 7차 미니 프로젝트 산출물 제출/10) AndroidAppSpringBoard.sql
@@ -30,12 +30,13 @@ CREATE TABLE MEMBER(
 --게시판
 DROP TABLE BOARD CASCADE CONSTRAINT;
 CREATE TABLE BOARD(
-  NO VARCHAR2(30),             		-- 게시글번호
+  NO NUMBER,      	     			-- 게시글번호
   ID VARCHAR2(20),            		-- 아이디
   TITLE VARCHAR2(100),        		-- 게시글제목
   CONTENTS VARCHAR2(1000),     		-- 게시글내용
-  DATES DATE DEFAULT SYSDATE,          	-- 게시글작성날짜
+  DATES DATE DEFAULT SYSDATE,       -- 게시글작성날짜
   HITS NUMBER DEFAULT 0,     		-- 조회수
+  IMAGE VARCHAR2(100), 				--이미지
   CONSTRAINT TB_BOARD_PK PRIMARY KEY(NO),
   CONSTRAINT TB_BOARD_FK_USER FOREIGN KEY(ID) REFERENCES MEMBER(ID) ON DELETE CASCADE -- 회원탈퇴를 하면 해당 회원이 쓴 게시글도 삭제
 );
