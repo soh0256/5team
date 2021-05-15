6c9846aa245edb03951703673dfa602a26ee5645
Park jiseon <soh0256@naver.com>
2021-05-15 20:40:51 +0900

    스프링 게시판 수정 재업

--- 개발 5팀 7차 미니 프로젝트 산출물 제출/android_server_spring/android_server/src/main/java/com/example/manager/common/UploadFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.example.manager.common;
-
-public class UploadFailureException extends RuntimeException {
-    public UploadFailureException(String message) {
-        super(message);
-    }
-}
